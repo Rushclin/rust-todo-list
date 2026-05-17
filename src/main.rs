fc06058da641f86184f4ebbde8ed4b373bcee368
Rushclin <takamrushclin@gmail.com>
2026-05-17 19:20:19 +0100

    install and configure dependancies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+use serde_json::json;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    name: String,
+    count: u8,
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let args = Args::parse();
+    let json = json!({
+        "name": "Takam"
+    });
+
+    println!("{:?}", args);
+    println!("{:?}", json);
+    println!("Le nom est {}", json["name"])
 }
